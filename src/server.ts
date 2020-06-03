adcbcbc5ded586fed35ba385248180fba9a1e30f
caosdan@gmail.com <caosdan@gmail.com>
2020-06-03 00:18:48 -0300

    implementacao de uma rota para verificar se o ambiente está configurado corretamente

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("listta de contas");
-  response.json(["danielson", "vanessa", "isadora", "dan1"]);
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json(["danielson", "vanessa", "isadora", "dan1"]);
 });
 
 app.listen(3333);
-
-/*
-para usar o typescript
-- instalar as definições de tipos: npm install @types/express
-- instalar o ts-node para poder executar os scripts no terminal: npm install ts-node
-- criar arquivo de configuração do typescript: npx tsc --init
-- para rodar um script: npx ts-node src/server.ts (npm run dev)
-- para monitorar modificações durante o desenvolvimento: npm install ts-node-dev -D
-*/
